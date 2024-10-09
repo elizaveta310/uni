7b4d7f2c5ab3b8f13fa6b1b5a95e7e5ace6e37c6
elizaveta310 <kravtsovaelizaveta31@gmail.com>
2024-10-09 09:42:48 +0200

    Create 4.2.4.3.cpp

---
+++ 4.2.4.3.cpp
@@ -0,0 +1,23 @@
+ #include <iostream>
+
+using namespace std;
+
+int main() {
+
+  int num1, num2, product;
+
+  cout << "Введіть перше число: ";
+
+  cin >> num1;
+
+  cout << "Введіть друге число: ";
+
+  cin >> num2;
+
+  product = num1 * num2;
+
+  cout << "Добуток: " << product << endl;
+
+  return 0;
+
+}
