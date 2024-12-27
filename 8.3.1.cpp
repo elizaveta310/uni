ad8b0c47ad98eec5bb54a31510c7cb826e73b1c4
elizaveta310 <kravtsovaelizaveta31@gmail.com>
2024-12-27 12:13:01 +0100

    Update 8.3.1.cpp

--- 8.3.1.cpp
+++ 8.3.1.cpp
@@ -10,20 +10,8 @@ double sum(double a, double b) {
 }
 
 int main() {
-  int a = 5, b = 7;
-  cout << "Сума двох цілих чисел: " << sum(a, b) << endl;
-
-  double x = 5.5, y = 7.7;
-  cout << "Сума двох подвійних значень: " << sum(x, y) << endl;
-
-  return 0;
-}
-
-
-int main() {
     int intResult = sum(5, 7); 
     cout << "Сума цілих чисел: " << intResult << endl;
-
   
     double doubleResult = sum(5.5, 7.5);  
     cout << "Сума подвійних чисел: " << doubleResult << endl;
