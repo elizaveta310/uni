e9094727a75caf9d531ce42d1fce90dba12f70fa
elizaveta310 <kravtsovaelizaveta31@gmail.com>
2024-12-27 11:28:36 +0100

    Create 8.2.1.cpp

---
+++ 8.2.1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void printNum(int number);
+
+int main() {
+    printNum(35);  
+    return 0;
+}
+
+void printNum(int number) {
+    cout << number;
+}
