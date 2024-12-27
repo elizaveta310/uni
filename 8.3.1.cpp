9933ff45106c71dd6725d55fd56fa6168321ede0
elizaveta310 <kravtsovaelizaveta31@gmail.com>
2024-12-27 12:12:12 +0100

    Update 8.3.1.cpp

--- 8.3.1.cpp
+++ 8.3.1.cpp
@@ -1,22 +1,32 @@
 #include <iostream>
 using namespace std;
 
-// Функція для обчислення суми двох цілих чисел
 int sum(int a, int b) {
   return a + b;
 }
 
-// Функція для обчислення суми двох подвійних значень
 double sum(double a, double b) {
   return a + b;
 }
 
 int main() {
-  int a = 5, b = 3;
+  int a = 5, b = 7;
   cout << "Сума двох цілих чисел: " << sum(a, b) << endl;
 
-  double x = 2.5, y = 1.7;
+  double x = 5.5, y = 7.7;
   cout << "Сума двох подвійних значень: " << sum(x, y) << endl;
 
   return 0;
+}
+
+
+int main() {
+    int intResult = sum(5, 7); 
+    cout << "Сума цілих чисел: " << intResult << endl;
+
+  
+    double doubleResult = sum(5.5, 7.5);  
+    cout << "Сума подвійних чисел: " << doubleResult << endl;
+
+    return 0;
 }
