8bb72ec6d21e20374fc6085d97e3574c9dbdb764
elizaveta310 <kravtsovaelizaveta31@gmail.com>
2024-12-30 13:25:06 +0100

    Update 8.4.2.cpp

--- 8.4.2.cpp
+++ 8.4.2.cpp
@@ -5,6 +5,7 @@
 using namespace std;
 
 int main() {
+    double calculatePi(int numn) {
     srand(time(0)); 
 
     int n; 
@@ -25,6 +26,18 @@ int main() {
     }
 
     cout << "Кількість дротиків всередині кола: " << insideCircle << endl;
+}
+
+ // Обчислення пі за формулою: 4 * (кількість_попадань_всередину / загальна_кількість_кидків)
+     double piEstimate = 4.0 * insideCircle / numKydkiv;
+    return piEstimate;
+}
+
+int main() {
+    int numKydkiv = 5000000; 
+    
+    double piValue = calculatePi(numKydkiv);
+    cout << "Приблизне значення числа пі: " << piValue << endl;  
 
     return 0;
 }
