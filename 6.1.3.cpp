1c3d0702fb83866c896e795f5844864bf5e8bd2d
elizaveta310 <kravtsovaelizaveta31@gmail.com>
2025-01-27 11:51:31 +0100

    Update 6.1.3.cpp

--- 6.1.3.cpp
+++ 6.1.3.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 
 void swapByPointer(int *a, int *b) {
-    int temp = *a;  
-    *a = *b;      
-    *b = temp;     
+    *a = *a + *b; 
+    *b = *a - *b;  
+    *a = *a - *b; 
 }
 
 int main() {
     int x = 5, y = 6;
     cout << "До обміну: x = " << x << ", y = " << y << endl;
-    swapByPointer(&x, &y); 
+    swapByPointer(&x, &y);
     cout << "Після обміну: x = " << x << ", y = " << y << endl;
     return 0;
 }
