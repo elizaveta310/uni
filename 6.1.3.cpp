38a1fc310e0eb7b6c8a322398d16948ca7d51ef3
elizaveta310 <kravtsovaelizaveta31@gmail.com>
2025-01-27 11:47:51 +0100

    Create 6.1.3.cpp

---
+++ 6.1.3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void swapByPointer(int *a, int *b) {
+    int temp = *a;  
+    *a = *b;      
+    *b = temp;     
+}
+
+int main() {
+    int x = 5, y = 6;
+    cout << "До обміну: x = " << x << ", y = " << y << endl;
+    swapByPointer(&x, &y); 
+    cout << "Після обміну: x = " << x << ", y = " << y << endl;
+    return 0;
+}
