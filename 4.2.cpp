c7d860f9c680e3f83dd25fee362d5f61ce86b1bc
elizaveta310 <kravtsovaelizaveta31@gmail.com>
2025-04-25 10:24:18 +0200

    Update 4.2.cpp

--- 4.2.cpp
+++ 4.2.cpp
@@ -8,102 +8,144 @@ struct Student {
     double averageGrade;
 };
 
-void printStudent(Student* student) {
-    cout << "Ім'я: " << student->name << endl;
-    cout << "Курс: " << student->course << endl;
-    cout << "Середній бал: " << student->averageGrade << endl;
-    cout << "----------------------" << endl;
-}
+struct Group {
+    string groupName;
+    int studentCount = 0;
+    Student* students = nullptr;
 
-int main() {
-    int n;
-
-    cout << "Введіть кількість студентів: ";
-    cin >> n;
-    cin.ignore();
-
-    Student* students = new Student[n];
-
-    if (students == nullptr) {
-        cerr << "Помилка виділення пам'яті!" << endl;
-        return 1;
+    void init_students() {
+        students = new Student[studentCount];
     }
 
-    for (int i = 0; i < n; ++i) {
-        cout << "Введіть дані для студента #" << i + 1 << ":" << endl;
-        cout << "Ім'я: ";
-        getline(cin, students[i].name);
+    void resize_students(int newCount) {
+        Student* newStudents = new Student[newCount];
 
-        cout << "Курс: ";
-        cin >> students[i].course;
-        cin.ignore(); 
+        for (int i = 0; i < min(studentCount, newCount); ++i) {
+            newStudents[i] = students[i];
+        }
 
-        cout << "Середній бал: ";
-        cin >> students[i].averageGrade;
-        cin.ignore(); 
-
-        cout << endl;
+        delete[] students;
+        students = newStudents;
+        studentCount = newCount;
     }
 
-    cout << "Список студентів:" << endl;
-    for (int i = 0; i < n; ++i) {
-        printStudent(&students[i]);
+    void printStudent(const Student& student) {
+        cout << "Ім'я: " << student.name << endl;
+        cout << "Курс: " << student.course << endl;
+        cout << "Середній бал: " << student.averageGrade << endl;
+        cout << "----------------------" << endl;
     }
 
-    Student* maxGradeStudent = &students[0];
-    for (int i = 1; i < n; ++i) {
-        if (students[i].averageGrade > maxGradeStudent->averageGrade) {
-            maxGradeStudent = &students[i];
+    void printAll() {
+        for (int i = 0; i < studentCount; ++i) {
+            printStudent(students[i]);
         }
     }
 
-    cout << "Студент з найбільшим середнім балом:" << endl;
-    printStudent(maxGradeStudent);
+    Student* getMaxAverageStudent() {
+        if (studentCount == 0) return nullptr;
 
-    Student* minCourseStudent = &students[0];
-    for (int i = 1; i < n; ++i) {
-        if (students[i].course < minCourseStudent->course) {
-            minCourseStudent = &students[i];
+        Student* maxStudent = &students[0];
+        for (int i = 1; i < studentCount; ++i) {
+            if (students[i].averageGrade > maxStudent->averageGrade) {
+                maxStudent = &students[i];
+            }
+        }
+        return maxStudent;
+    }
+
+    Student* getMinCourseStudent() {
+        if (studentCount == 0) return nullptr;
+
+        Student* minStudent = &students[0];
+        for (int i = 1; i < studentCount; ++i) {
+            if (students[i].course < minStudent->course) {
+                minStudent = &students[i];
+            }
+        }
+        return minStudent;
+    }
+
+    void searchByName(const string& name) {
+        bool found = false;
+        for (int i = 0; i < studentCount; ++i) {
+            if (students[i].name == name) {
+                printStudent(students[i]);
+                found = true;
+            }
+        }
+        if (!found) {
+            cout << "Студента з ім'ям '" << name << "' не знайдено." << endl;
         }
     }
 
-    cout << "Студент з найменшим курсом:" << endl;
-    printStudent(minCourseStudent);
-
-    string searchName;
-    cout << "Введіть ім'я для пошуку: ";
-    getline(cin, searchName);
-
-    cout << "Результати пошуку за ім'ям '" << searchName << "':" << endl;
-    bool found = false;
-    for (int i = 0; i < n; ++i) {
-        if (students[i].name == searchName) {
-            printStudent(&students[i]);
-            found = true;
-        }
-    }
-    if (!found) {
-        cout << "Студента з ім'ям '" << searchName << "' не знайдено." << endl;
-    }
-
-    for (int i = 0; i < n - 1; ++i) {
-        for (int j = 0; j < n - i - 1; ++j) {
-            if (students[j].course > students[j + 1].course) {
-                // Обмін елементів
-                Student temp = students[j];
-                students[j] = students[j + 1];
-                students[j + 1] = temp;
+    void sortByCourse() {
+        for (int i = 0; i < studentCount - 1; ++i) {
+            for (int j = 0; j < studentCount - i - 1; ++j) {
+                if (students[j].course > students[j + 1].course) {
+                    swap(students[j], students[j + 1]);
+                }
             }
         }
     }
 
-    cout << "Студенти, відсортовані за курсом:" << endl;
-    for (int i = 0; i < n; ++i) {
-        printStudent(&students[i]);
+    void clear() {
+        delete[] students;
+        students = nullptr;
+        studentCount = 0;
+    }
+};
+
+int main() {
+    Group group;
+    group.groupName = "ПІ-25";
+
+    cout << "Введіть кількість студентів: ";
+    cin >> group.studentCount;
+    cin.ignore();
+
+    group.init_students();
+
+    for (int i = 0; i < group.studentCount; ++i) {
+        cout << "Введіть дані для студента #" << i + 1 << ":" << endl;
+        cout << "Ім'я: ";
+        getline(cin, group.students[i].name);
+
+        cout << "Курс: ";
+        cin >> group.students[i].course;
+        cin.ignore();
+
+        cout << "Середній бал: ";
+        cin >> group.students[i].averageGrade;
+        cin.ignore();
+        cout << endl;
     }
 
-    delete[] students;
-    students = nullptr;
+    cout << "\nСписок студентів:" << endl;
+    group.printAll();
+
+    Student* topStudent = group.getMaxAverageStudent();
+    if (topStudent) {
+        cout << "\nСтудент з найбільшим середнім балом:" << endl;
+        group.printStudent(*topStudent);
+    }
+
+    Student* juniorStudent = group.getMinCourseStudent();
+    if (juniorStudent) {
+        cout << "\nСтудент з найменшим курсом:" << endl;
+        group.printStudent(*juniorStudent);
+    }
+
+    string searchName;
+    cout << "\nВведіть ім'я для пошуку: ";
+    getline(cin, searchName);
+    group.searchByName(searchName);
+
+    group.sortByCourse();
+    cout << "\nСтуденти, відсортовані за курсом:" << endl;
+    group.printAll();
+
+    group.clear();
 
     return 0;
 }
